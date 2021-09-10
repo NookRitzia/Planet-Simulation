c2d15124d8d56d64810a7ea55053a8ff8e236e91
NookRitzia <Edward.Stan.04@gmail.com>
2021-09-10 15:24:46 -0400

    Added waves
    Looks like a sinusoidial wave

---
+++ Planet Simulation/Assets/Scripts/Oscillation Scripts/SphereSpawner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereSpawner : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject entityToSpawn;
+    public int count;
+    public Shader shader;
+    void Start()
+    {
+        
+        for (int i = 1; i <= count; i++)
+        {
+            GameObject temp = entityToSpawn;
+            
+            temp.GetComponent<MeshRenderer>().material = new Material(shader);
+            temp.GetComponent<MeshRenderer>().material.color = new Color(i * 255f / count,i,i);
+            GameObject.Instantiate(temp, Vector3.right * ( 2f * i )/4, new Quaternion());
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Planet Simulation/Assets/Scripts/Oscillation Scripts/VerticalOscillationManager.cs
+++ Planet Simulation/Assets/Scripts/Oscillation Scripts/VerticalOscillationManager.cs
@@ -7,30 +7,39 @@ public class VerticalOscillationManager : MonoBehaviour
     private GameObject movingBody;
     public Vector3 center;
     public Vector3 initial;
+    public float delay = 0f;
     public float amplitude = 1f;
     public float accelerationCoeffecient = 1f;
     public float velocity = 0;
     public float acceleration = 0;
     private float displacement;
     private float distance;
+    private float startTime;
     void Start()
     {
         movingBody = this.gameObject;
-        movingBody.transform.Translate(new Vector3(0, amplitude));
-        displacement = initial.x - center.y;
+        displacement = initial.y - center.y;
         distance = Mathf.Abs(displacement);
-        velocity = Mathf.Sqrt(2 * accelerationCoeffecient*(amplitude - distance));
+        velocity = 0;
+        movingBody.transform.Translate(new Vector3(0, amplitude));
         if (displacement < 0)
             velocity *= -1;
-        movingBody.transform.position = initial;
+        //movingBody.transform.position = initial;
+        
+        startTime = Time.time;
+        delay = movingBody.transform.position.x / 10f;
+        startTime += 2;
     }
 
     // Update is called once per frame
     void Update()
     {
-        acceleration = -(movingBody.transform.position.y - center.y) * accelerationCoeffecient;
-        velocity += acceleration * Time.deltaTime;
-        gameObject.transform.Translate(new Vector3(0, velocity * Time.deltaTime));
-        //gameObject.transform.Translate(new Vector3(.2f,0,0));
+        if (Time.time - startTime > delay)
+        {
+            acceleration = -(movingBody.transform.position.y - center.y) * accelerationCoeffecient;
+            velocity += acceleration * Time.deltaTime;
+            gameObject.transform.Translate(new Vector3(0, velocity * Time.deltaTime));
+        }
+        gameObject.transform.Translate(new Vector3(0f,0,0));
     }
 }
